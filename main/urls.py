33d10bd26840c5b6f89dc0aa06aa5a09f2bf0db6
rhdprud <118238401+ZakZak0112@users.noreply.github.com>
2025-12-03 13:49:33 +0900

    [trash]

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
-# pickuplog/main/urls.py (최종 수정)
+# pickuplog/main/urls.py 
 
 from django.urls import path, include
-# 💡 필수: views 모듈을 임포트하여 아래 path() 함수에서 사용할 수 있게 합니다.
 from . import views 
 
 urlpatterns = [
@@ -12,7 +11,6 @@ urlpatterns = [
     path('insight/', views.insight_report, name='insight'),
     path('archive/lostitem/', views.lostitem_list, name='lostitem_list'),
     # 2. LostItem CRUD 및 아카이브 연결
-    path('archive/lostitem/', views.lostitem_list, name='lostitem_list'), 
     path('archive/lostitem/create/', views.lostitem_create, name='lostitem_create'), 
     path('archive/lostitem/update/<int:pk>/', views.lostitem_update, name='lostitem_update'),
     path('archive/lostitem/upload/csv/', views.lostitem_upload_csv, name='lostitem_upload_csv'), 
